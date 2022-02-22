aa9e42f460bda0c089aacc79eaa49545b2acff74
sirikoduru24 <siri4@pdx.edu>
2022-02-21 16:10:42 -0800

    nav bar added

--- frontEnd/worldofsuperheroes/src/App.js
+++ frontEnd/worldofsuperheroes/src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-
+import { BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
 import {useEffect, useState } from "react";
 
 function App() {
@@ -17,17 +17,32 @@ function App() {
     getData();
   }, [])
   return (
-    <div className="App">
-      <h1>Hello World!</h1>
-      {state.map((item, index) => {
-        const {name} = item;
-        return (
-          <div>
-            <h1>{name}</h1>
+    <Router>
+      <div>
+        <nav class="navbar navbar-expand-lg navbar-dark ">
+          <a class="navbar-brand inactiveLink"><h1 className="links">WORLD OF SUPER HEROS</h1></a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsingNavbarMd">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+          <div class="navbar-collapse collapse" id="collapsingNavbarMd">
+            <ul class="navbar-nav">
+              <li class="nav-item">
+                <Link to="/"><h4 className="links">HOME</h4></Link>
+              </li>
+              <li class="nav-item">
+                <Link to="/characters"><h4 className="links">CHARACTERS</h4></Link>
+              </li>
+              <li class="nav-item">
+                <Link to="/compare"><h4 className="links">COMPARE</h4></Link>
+              </li>
+              <li class="nav-item">
+                <Link to="/quiz"><h4 className="links">TAKE A QUIZ</h4></Link>
+              </li>
+            </ul>
           </div>
-        )
-      })}
+        </nav>
     </div>
+  </Router>
   );
 }
 
