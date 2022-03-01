08241601d057ed8d8e2e487ee3c9da7202db965f
sirikoduru24 <siri4@pdx.edu>
2022-02-28 16:48:04 -0800

    Quiz questions added

--- quizQuestions.js
+++ quizQuestions.js
@@ -1,39 +1,39 @@
-const qBank = [
-    {
-      question:
-        "how build the app ?",
-      answers: ["vinayak", "sarthak", "somil", "devesh"],
-      correct: "vinayak",
-      questionId: "099099"
-    },
-    {
-      question:
-        "how build the app ?",
-      answers: ["vinayak", "sarthak", "somil", "devesh"],
-      correct: "vinayak",
-      questionId: "093909"
-    },
-    {
-      question:
-        "how build the app ?",
-      answers: ["vinayak", "sarthak", "somil", "devesh"],
-      correct: "vinayak",
-      questionId: "009039"
-    },
-    {
-      question:
-        "how build the app ?",
-      answers: ["vinayak", "sarthak", "somil", "devesh"],
-      correct: "vinayak",
-      questionId: "090089"
-    },
-    {
-      question:
-        "how build the app ?",
-      answers: ["vinayak", "sarthak", "somil", "devesh"],
-      correct: "vinayak",
-      questionId: "01010101"
-    },
+const questions = [
+  {
+    questionText: 'What is the full name of Spiderman?',
+    answerOptions: [
+      { answerText: 'Peter Parker', isCorrect: true },
+      { answerText: 'Martha Franklin', isCorrect: false },
+      { answerText: 'Evan Daniels', isCorrect: false },
+      { answerText: 'Daniel RadCliff', isCorrect: false },
+    ],
+  },
+  {
+    questionText: 'What is the birth place of Captain America?',
+    answerOptions: [
+      { answerText: 'New Jersey City', isCorrect: false },
+      { answerText: 'New York City', isCorrect: true },
+      { answerText: 'Portland', isCorrect: false },
+      { answerText: 'Chicago', isCorrect: false },
+    ],
+  },
+  {
+    questionText: 'What is the intelligence of Superman on a scale of 100?',
+    answerOptions: [
+      { answerText: '94', isCorrect: true },
+      { answerText: '88', isCorrect: false },
+      { answerText: '66', isCorrect: false },
+      { answerText: '78', isCorrect: false },
+    ],
+  },
+  {
+    questionText: 'What is the occupation of Captain Cold?',
+    answerOptions: [
+      { answerText: 'Student', isCorrect: false },
+      { answerText: 'Lawyer', isCorrect: false },
+      { answerText: 'Criminal', isCorrect: true },
+      { answerText: 'Aeronautics', isCorrect: false },
+    ],
+  },
 ];
-
-module.exports = qBank;
+module.exports = questions;
