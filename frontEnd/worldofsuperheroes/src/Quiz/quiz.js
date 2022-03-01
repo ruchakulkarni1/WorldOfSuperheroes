7bcdf70a0061592bf6cc7a3bb0444d36f9eaadd1
sirikoduru24 <siri4@pdx.edu>
2022-02-28 17:57:48 -0800

    Added UI for Quiz

--- frontEnd/worldofsuperheroes/src/Quiz/quiz.js
+++ frontEnd/worldofsuperheroes/src/Quiz/quiz.js
@@ -1,8 +1,56 @@
-function Quiz(props) {
+import React, { useState } from 'react';
+
+function Quiz(props) { 
+    const [currentQuestion, setCurrentQuestion] = useState(0);
+	    const [showScore, setShowScore] = useState(false);
+	    const [score, setScore] = useState(0);
+        
     if(props) {
-        return(
-            <div>{props.data}</div>
-        )
+        const handleAnswerOptionClick = (isCorrect) => {
+            if (isCorrect) {
+                setScore(score + 1);
+                console.log(score);
+            }
+            const nextQuestion = currentQuestion + 1;
+            if (nextQuestion < props.data.length) {
+                setCurrentQuestion(nextQuestion);
+                console.log(currentQuestion);
+            } else {
+                setShowScore(true);
+            }
+        };
+        
+        const playAgain = () => {
+            setShowScore(0)
+            setCurrentQuestion(0);
+            setScore(0);
+        }
+        return (
+            <div className='quiz-app'>
+                {showScore ? (
+                    <div>
+                    <div className='score-section'>
+                        You scored {score} out of {props.data.length}
+                    </div>
+                    <div><button onClick={playAgain}>Play Again</button></div>
+                    </div>
+                ) : (
+                    <>
+                        <div className='question-section'>
+                            <div className='question-count'>
+                                <span>Question {currentQuestion + 1}</span>/{props.data.length}
+                            </div>
+                            <div className='question-text'>{props.data[currentQuestion].questionText}</div>
+                        </div>
+                        <div className='answer-section'>
+                            {props.data[currentQuestion].answerOptions.map((answerOption) => (
+                                <button onClick={() => handleAnswerOptionClick(answerOption.isCorrect)}>{answerOption.answerText}</button>
+                            ))}
+                        </div>
+                    </>
+                )}
+            </div>
+        );
     }
 }
 
