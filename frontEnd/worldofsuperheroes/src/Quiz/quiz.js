529e8f013c7d1726e49ccc31334df7436f545933
sirikoduru24 <siri4@pdx.edu>
2022-03-02 15:12:35 -0800

    Quiz look changed

--- frontEnd/worldofsuperheroes/src/Quiz/quiz.js
+++ frontEnd/worldofsuperheroes/src/Quiz/quiz.js
@@ -1,56 +1,143 @@
 import React, { useState } from 'react';
+import './quiz.css';
+import { useHistory } from "react-router-dom";
 
 function Quiz(props) { 
+    const history = useHistory();
     const [currentQuestion, setCurrentQuestion] = useState(0);
-	    const [showScore, setShowScore] = useState(false);
-	    const [score, setScore] = useState(0);
-        
-    if(props) {
-        const handleAnswerOptionClick = (isCorrect) => {
-            if (isCorrect) {
-                setScore(score + 1);
-                console.log(score);
-            }
-            const nextQuestion = currentQuestion + 1;
-            if (nextQuestion < props.data.length) {
-                setCurrentQuestion(nextQuestion);
-                console.log(currentQuestion);
-            } else {
-                setShowScore(true);
-            }
+        const [myAnswer, setMyAnswer] = useState("");
+        const [score, setScore] = useState(0);
+        const [finish, setFinish] = useState(false);
+        const [show, setShow] = useState(false);
+        const [clickAnswer, setClickAnswer] = useState(false);
+          
+    if(props.data) {
+        console.log('Here',props.filterData);
+        const checkAnswer = (variant) => {
+          setMyAnswer(variant);
+          setClickAnswer(true);
+        };
+      
+        const checkCorrectAnswer = () => {
+          if (myAnswer === props.data[currentQuestion].answer) {
+            setScore(score + 1);
+          }
+        };
+      
+        const showAnswer = () => {
+          setShow((show) => !show);
+        };
+        const reset = () => {
+          setShow(false);
+          setClickAnswer(false);
+        };
+      
+        const finishHandler = () => {
+          if (currentQuestion === props.data.length - 1) {
+            setFinish(true);
+          }
+        };
+      
+        const startOver = () => {
+          setCurrentQuestion(0);
+          setFinish(false);
+          setMyAnswer("");
+          setScore(0);
+          setShow(false);
+          history.push("/quiz");
         };
         
-        const playAgain = () => {
-            setShowScore(0)
-            setCurrentQuestion(0);
-            setScore(0);
+        const mainfunc = () => {
+                return (
+                    <div class="wrapper">
+                    <p class="textclass">
+                      {props.data[currentQuestion].question}
+                    </p>
+                    <span class="m-2 border-2 border-black mx-auto px-2 bg-gray text-pink-400 rounded-lg text-center">
+                      {`${currentQuestion+1}/${props.data.length}`}
+                    </span>
+                    {props.data[currentQuestion].variants.map((variant) => (
+                      <div class="blockdesign">
+                        <p
+                          key={variant.id}
+                          class={`variant ${
+                            myAnswer === variant
+                              ? myAnswer === props.data[currentQuestion].answer
+                                ? "correctAnswer"
+                                : "incorrectAnswer"
+                              : null
+                          }`}
+                          onClick={() => checkAnswer(variant)}
+                        >
+                          {variant}
+                        </p>
+                      </div>
+                    ))}
+          
+                    {clickAnswer && (
+                      <button class = "showAnsBtn"
+                        onClick={() => showAnswer()}
+                      >
+                        SHOW ANSWER
+                      </button>
+                    )}
+                    {show && (
+                      <p class="showAnswer">
+                        Correct Answer: {props.data[currentQuestion].answer}
+                      </p>
+                    )}
+          
+                    {currentQuestion < props.data.length -1 && (
+                      <button
+                        class="nextButton"
+                        onClick={() => {
+                          setCurrentQuestion(currentQuestion + 1);
+                          checkCorrectAnswer();
+                          reset();
+                        }}
+                      >
+                        NEXT
+                      </button>
+                    )}
+          
+                    {currentQuestion === props.data.length-1  && (
+                      <button
+                        class="finishButton"
+                        onClick={() => finishHandler()}
+                      >
+                        FINISH
+                      </button>
+                    )}
+                  </div>
+                )
         }
-        return (
-            <div className='quiz-app'>
-                {showScore ? (
-                    <div>
-                    <div className='score-section'>
-                        You scored {score} out of {props.data.length}
-                    </div>
-                    <div><button onClick={playAgain}>Play Again</button></div>
-                    </div>
-                ) : (
-                    <>
-                        <div className='question-section'>
-                            <div className='question-count'>
-                                <span>Question {currentQuestion + 1}</span>/{props.data.length}
-                            </div>
-                            <div className='question-text'>{props.data[currentQuestion].questionText}</div>
-                        </div>
-                        <div className='answer-section'>
-                            {props.data[currentQuestion].answerOptions.map((answerOption) => (
-                                <button onClick={() => handleAnswerOptionClick(answerOption.isCorrect)}>{answerOption.answerText}</button>
-                            ))}
-                        </div>
-                    </>
-                )}
+        if (finish) {
+          return (
+            <div class="maindivquiz">
+              <div class="wrapper">
+                <h3 class="textclass">
+                  {`Game Over!
+                  Your Final Score is
+                  ${score}/${props.data.length}
+                  points.`}
+                </h3>
+                <button
+                  class= "nextButton"
+                  onClick={() => startOver()}
+                >
+                  Start Over
+                </button>
+              </div>
             </div>
-        );
+          );
+        } else {
+          return (
+            <div class="maindivquiz">
+                <div>{mainfunc()}</div>
+            </div>
+          );
+        }
+      
     }
 }
 
