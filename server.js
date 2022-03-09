c877b1aef7f470bd5e5b0394d89ab5c5b3e0fc89
Rucha Kulkarni <rucha2@pdx.edu>
2022-03-08 16:52:17 -0800

    Reverted changes from server.js

--- server.js
+++ server.js
@@ -5,19 +5,9 @@ const data = require("./apiData");
 const quizData = require("./quizQuestions");
 const { applyEach } = require("async");
 const app = express();
-<<<<<<< HEAD
-const path = require("path");
-=======
-const PORT = process.env.PORT || 5000
->>>>>>> 38129a9481852ed49323285d1704b3218025995c
 
 app.use(cors());
 app.use(express.static('public'));
-app.use(express.static(path.resolve(__dirname, "./frontEnd/worldofsuperheroes/build")));
-
-app.get("*", function (request, response) {
-  response.sendFile(path.resolve(__dirname, "./frontEnd/worldofsuperheroes/build", "index.html"));
-});
 
 app.get("/api", (req, res) => {
     res.json(data);
@@ -28,6 +18,6 @@ app.get("/quizData",(req,res) => {
     res.json(quizData);
 });
 
-app.listen(PORT, () => {
+app.listen(5000, () => {
     console.log("server is running on port 5000.");
 })
