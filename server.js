9fb49c85572a4432468b3bbeaf3067a67286c8ee
Rucha Kulkarni <rucha2@pdx.edu>
2022-03-08 22:12:43 -0800

    Added path in serverjs and app.use -  11 by Rucha

--- server.js
+++ server.js
@@ -9,6 +9,9 @@ const app = express();
 app.use(cors());
 app.use(express.static('public'));
 
+const path = require("path")
+app.use(express.static(path.join(__dirname, "frontEnd/worldofsuperheroes", "build")))
+
 app.get("/", (req, res) => {
     console.log("Server is running.. Hi there!!")
     res.json(data);
@@ -21,6 +24,10 @@ app.get("/api", (req, res) => {
 app.get("/quizData",(req,res) => {
     console.log(quizData);
     res.json(quizData);
+});
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "frontEnd/worldofsuperheroes", "build", "index.html"));
 });
 
 app.listen(process.env.PORT || 5000, () => {
