73bb0c2501dcce8a5028c99b591972f7abfc2ec1
Rucha Kulkarni <rucha2@pdx.edu>
2022-03-08 17:42:51 -0800

    Server Port changes by Rucha

--- server.js
+++ server.js
@@ -18,6 +18,6 @@ app.get("/quizData",(req,res) => {
     res.json(quizData);
 });
 
-app.listen(5000, () => {
+app.listen(process.env.PORT || 5000, () => {
     console.log("server is running on port 5000.");
 })
